6a8fd460a240abd4e3b6a65612a1644901390b27
mc <alfonso.perezembid.atalaya@gmail.com>
2013-12-23 00:56:06 +0100

    hola

---
+++ aconode.py
@@ -0,0 +1,20 @@
+
+
+class ACONode():
+    
+    ''' Class for a ACO Node '''
+    
+    def __init__(self, state, solution=False, start=False):
+        
+        self.state = state
+        self.solution = solution
+        self.start = start
+        
+    def __eq__(self,other):
+        
+        return (isinstance(other, ACONode) and self.state == other.state)
+    
+    def __ne__(self,other):
+        
+        return not(self.__eq__(other))
+    
